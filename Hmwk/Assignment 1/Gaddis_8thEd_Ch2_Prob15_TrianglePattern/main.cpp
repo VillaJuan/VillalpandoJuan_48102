82a64f9f026b07850f824d65c0a1a52d4f6b4305
villajuan <villalpando.juan00@gmail.com>
2016-09-22 00:13:24 -0700

    Signed-off-by: villajuan <villalpando.juan00@gmail.com>

---
+++ Hmwk/Assignment 1/Gaddis_8thEd_Ch2_Prob15_TrianglePattern/main.cpp
@@ -0,0 +1,34 @@
+/* 
+   File:   main
+   Author: Juan F. Villalpando
+   Created on September 21, 2016, 9:49 PM
+   Purpose:  To create a triangular pattern
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+    
+    //Input values
+    
+    //Process values -> Map inputs to Outputs
+    
+    //Display Output
+cout<<"   *   "<<endl;
+cout<<"  ***  "<<endl;
+cout<<" ***** "<<endl;
+cout<<"*******"<<endl;
+        
+    //Exit Program
+    return 0;
+}
